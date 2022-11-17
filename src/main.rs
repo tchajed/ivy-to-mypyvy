b732d5dd2bdbae4b39605f358c5042441604d9a6
Tej Chajed <tchajed@vmware.com>
2022-11-17 14:13:08 -0800

    Get some parsing and CLI working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate pest;
+
+// import only for trait
+use clap::Parser as _;
+use pest::Parser as _;
+use std::fs;
+
+#[derive(pest_derive::Parser)]
+#[grammar = "ivy.pest"]
+pub struct IvyParser;
+
+#[derive(clap::Parser, Debug)]
+#[command(about, long_about=None)]
+struct Args {
+    /// Filename for a file with l2s_debug=true output
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let unparsed_file = fs::read_to_string(args.file).expect("could not read input file");
+
+    let file = IvyParser::parse(Rule::file, &unparsed_file)
+        .expect("unsuccessful parse")
+        .next()
+        .unwrap();
+
+    println!("{:?}", file);
 }
