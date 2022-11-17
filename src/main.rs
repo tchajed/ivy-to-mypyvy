39ad426ec59787c2c76c197194b16dbfd7ce0ba7
Tej Chajed <tchajed@vmware.com>
2022-11-17 15:15:47 -0800

    Finish parsing example input

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,8 @@ fn main() {
     IvyParser::parse(Rule::expr, "forall V0.  l2s_a(V0)").expect("unsuccessful expr parse");
     IvyParser::parse(Rule::expr, "l2s_g_1 -> ~(forall T. mutex_protocol.d(T))")
         .expect("unsuccessful expr parse");
-    IvyParser::parse(Rule::assume_rule, "assume l2s_g_1 -> ~(forall T. mutex_protocol.d(T))")
-        .expect("unsuccessful assume parse");
+    IvyParser::parse(Rule::forall_expr, "forall T. mutex_protocol.d(T)")
+        .expect("unsuccessful forall parse");
     IvyParser::parse(Rule::expr, "forall V0. l2s_g_4(V0) -> ~l2s_g_3(V0)")
         .expect("unsuccessful expr parse");
 
@@ -44,5 +44,7 @@ fn main() {
     let _pratt = PrattParser::new()
         .op(Op::infix(Rule::and, Assoc::Left))
         .op(Op::infix(Rule::implies, Assoc::Left))
+        .op(Op::infix(Rule::iff, Assoc::Left))
+        .op(Op::infix(Rule::equal, Assoc::Left))
         .op(Op::prefix(Rule::not));
 }
