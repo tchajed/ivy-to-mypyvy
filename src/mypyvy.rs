69d8200601a2c66706c0e006cd925b5c332749b2
Tej Chajed <tchajed@vmware.com>
2022-11-18 10:13:52 -0800

    Remove Indentation
    This was a bit over-engineered, I don't think we'll really need it

--- src/mypyvy.rs
+++ src/mypyvy.rs
@@ -1,47 +1,8 @@
-use std::{fmt, io};
+use std::io;
 
 use crate::ivy_l2s::{Transition, Transitions};
 
-struct Indentation<'a, W>
-where
-    W: io::Write,
-{
-    _indent: usize,
-    writer: &'a mut W,
-}
-
-impl<'a, W> Indentation<'a, W>
-where
-    W: io::Write,
-{
-    fn new(w: &'a mut W) -> Self {
-        Indentation {
-            _indent: 0,
-            writer: w,
-        }
-    }
-
-    fn indent(&mut self, amount: usize) {
-        self._indent += amount;
-    }
-
-    fn unindent(&mut self, amount: usize) {
-        assert!(
-            self._indent >= amount,
-            "unindent exceeds current indentation"
-        );
-        self._indent -= amount;
-    }
-
-    fn write_fmt(&mut self, fmt: fmt::Arguments<'_>) -> io::Result<()> {
-        write!(self.writer, "{:width$}", "", width = self._indent)?;
-        self.writer.write_fmt(fmt)?;
-        Ok(())
-    }
-}
-
 fn transition(w: &mut impl io::Write, t: &Transition) -> io::Result<()> {
-    let mut w = Indentation::new(w);
     let args = match &t.bound {
         // TODO: arg actually has type here
         Some(arg) => format!("({}: ???)", arg),
@@ -49,12 +10,10 @@ fn transition(w: &mut impl io::Write, t: &Transition) -> io::Result<()> {
     };
     writeln!(w, "transition {}{}", t.name, args)?;
 
-    w.indent(2);
     // TODO: process steps
     for step in &t.steps {
-        writeln!(w, "{:?}", step)?;
+        writeln!(w, "  {:?}", step)?;
     }
-    w.unindent(2);
     writeln!(w)?;
     Ok(())
 }
