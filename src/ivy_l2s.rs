ec0897e65758b9bf399fc477d3c9c996da8d6a83
Tej Chajed <tchajed@vmware.com>
2022-11-19 14:50:15 -0800

    Re-arrange some things for clarity

--- src/ivy_l2s.rs
+++ src/ivy_l2s.rs
@@ -139,20 +139,6 @@ fn parse_relation(pair: Pair<Rule>) -> Relation {
     }
 }
 
-fn make_pratt() -> PrattParser<Rule> {
-    // these operators should be from lowest priority to highest
-    // see https://kenmcmil.github.io/ivy/language.html#expressions
-    PrattParser::new()
-        // implies is actually left-associative (considered a bug in Ivy)
-        //
-        // therefore p -> q -> r means (p -> q) -> r (which is unintuitive)
-        .op(Op::infix(Rule::equal, Assoc::Left))
-        .op(Op::prefix(Rule::not))
-        .op(Op::infix(Rule::and, Assoc::Left))
-        .op(Op::infix(Rule::or, Assoc::Left))
-        .op(Op::infix(Rule::implies, Assoc::Left) | Op::infix(Rule::iff, Assoc::Left))
-}
-
 fn parse_quantifer(quantifier: Pair<Rule>) -> Quantifier {
     match quantifier.as_rule() {
         Rule::forall => Quantifier::Forall,
@@ -180,6 +166,20 @@ fn parse_base_expr(expr: Pair<Rule>) -> Expr {
         Rule::expr => parse_expr(expr),
         _ => unreachable!(),
     }
+}
+
+fn make_pratt() -> PrattParser<Rule> {
+    // these operators should be from lowest priority to highest
+    // see https://kenmcmil.github.io/ivy/language.html#expressions
+    PrattParser::new()
+        .op(Op::infix(Rule::equal, Assoc::Left))
+        .op(Op::prefix(Rule::not))
+        .op(Op::infix(Rule::and, Assoc::Left))
+        .op(Op::infix(Rule::or, Assoc::Left))
+        // implies is actually left-associative (considered a bug in Ivy)
+        //
+        // therefore p -> q -> r means (p -> q) -> r (which is unintuitive)
+        .op(Op::infix(Rule::implies, Assoc::Left) | Op::infix(Rule::iff, Assoc::Left))
 }
 
 fn parse_expr(expr: Pair<Rule>) -> Expr {
