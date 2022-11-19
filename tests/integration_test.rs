35a4c08ebff3f2dde7f6a70e51a37b5ac45066bd
Tej Chajed <tchajed@vmware.com>
2022-11-19 14:32:04 -0800

    Add a test for Ivy output

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,17 +2,21 @@ use std::fs;
 
 use ivy_to_mypyvy::{
     ivy_l2s::{self, System},
-    mypyvy,
+    mypyvy, pretty,
 };
 
 fn read_input() -> String {
     fs::read_to_string("tests/l2s.out").expect("could not read input file")
 }
 
+fn parse_input() -> System {
+    let input = read_input();
+    ivy_l2s::parse(&input).expect("unsuccessful parse of input file")
+}
+
 #[test]
 fn mutex_l2s_output_parses() {
-    let input = read_input();
-    let sys = ivy_l2s::parse(&input).expect("unsuccessful parse of input file");
+    let sys = parse_input();
     assert_eq!(
         sys.transitions.len(),
         7,
@@ -32,8 +36,20 @@ fn mypyvy_output(file: &System) -> String {
 
 #[test]
 fn mutex_l2s_file() {
-    let input = read_input();
-    let file = ivy_l2s::parse(&input).expect("unsuccessful parse of input file");
-    let output = mypyvy_output(&file);
+    let sys = parse_input();
+    let output = mypyvy_output(&sys);
+    insta::assert_display_snapshot!(output);
+}
+
+fn ivy_output(sys: &System) -> String {
+    let mut buf = vec![];
+    pretty::print_system(&mut buf, sys);
+    String::from_utf8(buf).expect("output is invalid utf-8")
+}
+
+#[test]
+fn mutex_l2s_ivy_pretty_print() {
+    let sys = parse_input();
+    let output = ivy_output(&sys);
     insta::assert_display_snapshot!(output);
 }
