b871e4446876dfbc188b2993969a1d54b8e3a1ef
Tej Chajed <tchajed@vmware.com>
2022-12-05 14:49:49 -0800

    Add better mutex as an integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,18 +5,19 @@ use ivy_to_mypyvy::{
     mypyvy, pretty,
 };
 
-fn read_input() -> String {
-    fs::read_to_string("tests/l2s.out").expect("could not read input file")
+fn read_input(name: &str) -> String {
+    fs::read_to_string(format!("tests/{name}")).expect("could not read input file")
 }
 
-fn parse_input() -> System {
-    let input = read_input();
-    ivy_l2s::parse(&input).expect("unsuccessful parse of input file")
+fn parse_input(name: &str) -> System {
+    let input = read_input(name);
+    ivy_l2s::parse(&input)
+        .unwrap_or_else(|err| panic!("unsuccessful parse of input file {name}: {err}"))
 }
 
 #[test]
 fn mutex_l2s_output_parses() {
-    let sys = parse_input();
+    let sys = parse_input("mutex.l2s.out");
     assert_eq!(
         sys.transitions.len(),
         8,
@@ -30,14 +31,21 @@ fn mutex_l2s_output_parses() {
 
 #[test]
 fn mutex_l2s_file() {
-    let sys = parse_input();
+    let sys = parse_input("mutex.l2s.out");
+    let output = mypyvy::fmt_system(&sys);
+    insta::assert_display_snapshot!(output);
+}
+
+#[test]
+fn better_mutex_l2s_file() {
+    let sys = parse_input("better_mutex.l2s.out");
     let output = mypyvy::fmt_system(&sys);
     insta::assert_display_snapshot!(output);
 }
 
 #[test]
 fn mutex_l2s_ivy_pretty_print() {
-    let sys = parse_input();
+    let sys = parse_input("mutex.l2s.out");
     let output = pretty::fmt_system(&sys);
     insta::assert_display_snapshot!(output);
 }
